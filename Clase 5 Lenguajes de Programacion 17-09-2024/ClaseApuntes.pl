042d18239f19a174ed69fc7025047b53ad3b4d15
Emilio-Aldean <emilio.aldean@gmail.com>
2024-09-17 07:37:47 -0500

    Prueba 2

--- Clase 5 Lenguajes de Programacion 17-09-2024/ClaseApuntes.pl
+++ Clase 5 Lenguajes de Programacion 17-09-2024/ClaseApuntes.pl
@@ -40,3 +40,45 @@ conjugar_verbo(Verbo, Tiempo, Persona, Numero, Conjugacion):-
     Conjugacion = Verbo
     ).
 
+
+tiempo(presente).
+tiempo(pasado).
+tiempo(futuro).
+
+persona(primera).
+persona(segunda).
+persona(tercera).
+
+numero(singular).
+numero(plural).
+
+ser(presente, primera, singular, "play").
+ser(presente, segunda, singular, "play").
+ser(presente, tercera, singular, "play").
+ser(presente, primera, plural, "play").
+ser(presente, segunda, plural, "play").
+ser(presente, tercera, plural, "play").
+
+ser(pasado, primera, singular, "played").
+ser(pasado, segunda, singular, "played").
+ser(pasado, tercera, singular, "played").
+ser(pasado, primera, plural, "played").
+ser(pasado, segunda, plural, "played").
+ser(pasado, tercera, plural, "played").
+
+ser(futuro, primera, singular, "will play").
+ser(futuro, segunda, singular, "will play").
+ser(futuro, tercera, singular, "will play").
+ser(futuro, primera, plural, "will play").
+ser(futuro, segunda, plural, "will play").
+ser(futuro, tercera, plural, "will play").
+
+conjugar_verbo(Verbo, Tiempo, Persona, Numero, Conjugacion):-
+    tiempo(Tiempo),
+    persona(Persona),
+    numero(Numero),
+    (   
+    	Verbo = "to play" ->  ser(Tiempo,Persona,Numero,R),
+        Conjugacion = R; 
+    Conjugacion = Verbo
+    ).
